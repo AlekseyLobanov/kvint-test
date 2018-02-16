83c45ef1adf47321342bd8d0419e1a2be3dcb647
Aleksey Lobanov <i@likemath.ru>
2018-02-16 20:03:34 +0300

    Added tests

---
+++ test_core.py
@@ -0,0 +1,32 @@
+import core
+
+def test_simple():
+    """Тест из спецификации"""
+    uid = "tester"
+    assert core.dialog.proc(uid, "/start") == "Какую вы хотите пиццу? Большую или маленькую?"
+    assert core.dialog.proc(uid, "Большую") == "Как вы будете платить?"
+    assert core.dialog.proc(uid, "Наличкой") == "Вы хотите большую пиццу, оплата - наличкой?"
+    assert core.dialog.proc(uid, "Да") == "Спасибо за заказ"
+
+
+def test_variation():
+    """Если фразы немного поменяются"""
+    uid = "tester2"
+    assert core.dialog.proc(uid, "/start") == "Какую вы хотите пиццу? Большую или маленькую?"
+    assert core.dialog.proc(uid, "большую") == "Как вы будете платить?"
+    assert core.dialog.proc(uid, "наличными") == "Вы хотите большую пиццу, оплата - наличкой?"
+    assert core.dialog.proc(uid, "YES!") == "Спасибо за заказ"
+
+
+def test_two_users():
+    """Возможно ли обрабатывать сразу двух пользователей"""
+    uid = "tester3"
+    uid2 = "tester4"
+    assert core.dialog.proc(uid, "/start") == "Какую вы хотите пиццу? Большую или маленькую?"
+    assert core.dialog.proc(uid, "Большую") == "Как вы будете платить?"
+    assert core.dialog.proc(uid2, "/start") == "Какую вы хотите пиццу? Большую или маленькую?"
+    assert core.dialog.proc(uid2, "большую") == "Как вы будете платить?"
+    assert core.dialog.proc(uid, "Наличкой") == "Вы хотите большую пиццу, оплата - наличкой?"
+    assert core.dialog.proc(uid, "Да") == "Спасибо за заказ"
+    assert core.dialog.proc(uid2, "наличными") == "Вы хотите большую пиццу, оплата - наличкой?"
+    assert core.dialog.proc(uid2, "YES!") == "Спасибо за заказ"
