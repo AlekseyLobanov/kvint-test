4438d6ed88bda88691d91f60b85ca6a24489b583
Aleksey Lobanov <i@likemath.ru>
2018-02-16 17:59:10 +0300

    Echo test

--- bot.py
+++ bot.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 import os
+import time
+
 import telebot
 
 TELEGRAM_API_TOKEN = os.environ['TELEGRAM_TOKEN']
 print("Using token {}".format(TELEGRAM_API_TOKEN[-4:])
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_msg(message):
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
